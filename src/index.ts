3e9b1370c6a36f169f4c6c89519ee911ddbad07d
Yesukay <yesukay2@gmail.com>
2025-05-29 14:37:36 -0400

    code  cleanup

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,3 @@
-// import moment from "moment";
-// import "./index.scss";
-// import tools from "./tools.json";
-
-// const dateContainer: HTMLElement = document.querySelector("time")!;
-// const mainContainer: HTMLElement = document.querySelector("main")!;
-// const buttonImage: HTMLImageElement = document.querySelector("button img")!;
-
-// buttonImage.addEventListener("click", () => {
-//   document.body.classList.toggle("dark");
-
-//   if (document.body.classList.contains("dark")) {
-//     buttonImage.src =
-//       "https://img.icons8.com/external-linear-outline-icons-papa-vector/78/external-Light-Mode-interface-linear-outline-icons-papa-vector.png";
-//     buttonImage.width = 30;
-//     buttonImage.height = 30;
-//   } else {
-//     buttonImage.src =
-//       "https://img.icons8.com/sf-ultralight-filled/25/moon-symbol.png";
-//   }
-// });
-
-// setInterval(() => {
-//   dateContainer.innerHTML = moment().format("DD-MM-YYYY HH:mm:ss");
-// }, 1000);
-
-// mainContainer.innerHTML = `${tools.tools
-//   .map((tool: string) => {
-//     return `<li class="tool">${tool}</li> <br />`;
-//   })
-//   .join("")}`;
-
-// index.ts
 import { getCurrentTime, generateToolListHTML } from "./utils";
 import tools from "./tools.json";
 import "./index.scss";
